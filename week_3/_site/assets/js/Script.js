3fdefe2a3cd4ed28a819268e5f47dae10889a533
d.bitter <d.bitter@viriciti.nl>
2017-02-28 11:24:43 +0100

    final fixes

---
+++ week_3/_site/assets/js/Script.js
@@ -0,0 +1,53 @@
+/*global window */
+(function () {
+    "use strict";
+    var app = {
+        init: function (routes, sections, window) {
+            routes.init(sections, window);
+        }
+    };
+    var routes = {
+        init: function (sections, window) {
+            window.onhashchange = function () {
+                sections.toggle();
+            };
+
+            document.getElementById("menu-button").addEventListener("click", function () {
+                document.getElementById("menu").classList.toggle("is-open");
+            });
+
+            document.getElementById("picture-fig").addEventListener("click", function () {
+                document.getElementById("modal").classList.toggle("active");
+            });
+
+            document.querySelector(".close-modal").addEventListener("click", function () {
+                document.getElementById("modal").classList.toggle("active");
+            });
+        }
+    };
+    var sections = {
+        toggle: function () {
+
+            var sectionList = document.querySelectorAll("section");
+
+            sectionList.forEach(function (sec) {
+                if (location.hash === "#all") {
+                    sec.classList.add("active");
+                    document.querySelector("a[href='#all']").classList.add("active");
+                    document.querySelector("a[href='#" + sec.id + "']").classList.remove("active");
+                } else if (location.hash === "#" + sec.id) {
+                    sec.classList.add("active");
+                    document.querySelector("a[href='#" + sec.id + "']").classList.add("active");
+                } else {
+                    sec.classList.remove("active");
+                    document.querySelector("a[href='#" + sec.id + "']").classList.remove("active");
+                    document.querySelector("a[href='#all']").classList.remove("active");
+                }
+
+            });
+
+            document.getElementById("menu").classList.toggle("is-open");
+        }
+    };
+    app.init(routes, sections, window);
+}());
